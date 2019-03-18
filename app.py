052607b02a4c845d62ec88f53728a9c324e645d0
Caleb Neel <me@calebneel.com>
2019-03-18 09:10:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import json
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/speedsandfeedsendpoint')
+def calulatue():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
   
 materals = """{
   "Carbide": {
